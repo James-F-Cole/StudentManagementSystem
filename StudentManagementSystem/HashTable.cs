e68f65e4dcc771857aa87e4dba3ea003d28a55d1
James Cole <colej7558@gmail.com>
2026-07-14 12:47:15 +0000

    Implement hash table backend and CRUD operations

---
+++ StudentManagementSystem/HashTable.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+
+namespace StudentManagementSystem
+{
+    internal class HashTable
+    {
+
+        private StudentRecord[] studentRecords = new StudentRecord[10];
+
+        public int Hash(int key)
+        {
+            int _arrayLength = studentRecords.Length;
+            int hashOutput = key % _arrayLength;
+            return hashOutput;
+        }
+
+        public bool Contains(int key)
+        {
+            int hashValue = Hash(key);
+            StudentRecord previousStudent = null;
+            StudentRecord currentStudent = this.studentRecords[hashValue];
+            while (currentStudent != null)
+            {
+                if (currentStudent.StudentId == key)
+                {
+                    return true;
+                }
+                previousStudent = currentStudent;
+                currentStudent = currentStudent.Next;
+            }
+            return false;
+
+        }
+
+        public bool Add(StudentRecord value)
+        {
+            int hashValue = Hash(value.StudentId);
+            if (studentRecords[hashValue] is StudentRecord)
+            {
+                if (!Contains(value.StudentId))
+                {
+                    StudentRecord currentStudent = this.studentRecords[hashValue];
+                    while (currentStudent.Next is StudentRecord)
+                    {
+                        currentStudent = currentStudent.Next;
+                    }
+                    currentStudent.Next = value;
+                    return true;
+                }
+                return false;
+            }
+            else
+            {
+                studentRecords[hashValue] = value;
+                return true;
+            }
+        }
+
+        public bool Remove(int key)
+        {
+            int hashValue = Hash(key);
+            StudentRecord previousStudent = null;
+            StudentRecord currentStudent = this.studentRecords[hashValue];
+            while (currentStudent is StudentRecord)
+            {
+
+                if (currentStudent.StudentId == key)
+                {
+                    if (previousStudent == null)
+                    {
+                        this.studentRecords[hashValue] = currentStudent.Next;
+                        return true;
+                    }
+                    previousStudent.Next = currentStudent.Next;
+                    return true;
+                }
+                previousStudent = currentStudent;
+                currentStudent = currentStudent.Next;
+            }
+            return false;
+        }
+
+        public StudentRecord Search(int key)
+        {
+            int hashValue = Hash(key);
+            StudentRecord previousStudent = null;
+            StudentRecord currentStudent = this.studentRecords[hashValue];
+            while (currentStudent is StudentRecord)
+            {
+
+                if (currentStudent.StudentId == key)
+                {
+                    return currentStudent;
+                }
+                previousStudent = currentStudent;
+                currentStudent = currentStudent.Next;
+            }
+            return null;
+        }
+
+        public bool Update(StudentRecord student, string fullName, DateTime dateOfBirth, string phoneNumber, string emailAddress, string major)
+        {
+            student.FullName = fullName;
+            student.DateOfBirth = dateOfBirth;
+            student.PhoneNumber = phoneNumber;
+            student.EmailAddress = emailAddress;
+            student.Major = major;
+            return true;
+        }
+
+        public string ViewAll()
+        {
+            string output = $"";
+            foreach (var record in this.studentRecords)
+            {
+                if (record is StudentRecord)
+                {
+                    output += "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+                    StudentRecord currentStudent = record;
+                    while (currentStudent is StudentRecord)
+                    {
+                        output += currentStudent.View() + "\n";
+                        currentStudent = currentStudent.Next;
+                    }
+                }
+            }
+            return output;
+        }
+    }
+}
---
+++ StudentManagementSystem/StudentRecord.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementSystem
+{
+    internal class StudentRecord
+    {
+        private int _studentId;
+        public int StudentId { get { return _studentId; } set { _studentId = value; } }
+
+        private string _fullName;
+        public string FullName { get { return _fullName; } set { _fullName = value; } }
+
+        private DateTime _dateOfBirth;
+        public DateTime DateOfBirth { get { return _dateOfBirth; } set { _dateOfBirth = value; } }
+
+        private string _phoneNumber;
+        public string PhoneNumber { get { return _phoneNumber; } set { _phoneNumber = value; } }
+
+        private string _emailAddress;
+        public string EmailAddress { get { return _emailAddress; } set { _emailAddress = value; } }
+
+        /*
+        private string _department;
+        public string Department { get { return _department; } set { _department = value; } }
+        */
+        private string _major;
+        public string Major { get { return _major; } set { _major = value; } }
+
+        private StudentRecord _next;
+        public StudentRecord Next { get { return _next; } set { _next = value; } }
+
+        public StudentRecord(int studentID, string fullName, DateTime dateOfBirth, string phoneNumber, string emailAddress, string major)
+        {
+
+            this.StudentId = studentID;
+            this.FullName = fullName;
+            this.DateOfBirth = dateOfBirth;
+            this.PhoneNumber = phoneNumber;
+            this.EmailAddress = emailAddress;
+            this.Major = major;
+            this.Next = null;
+
+        }
+
+        public string View()
+        {
+            return $"ID: {this.StudentId}  |  Full Name: {this.FullName}  |  Date Of Birth: {this.DateOfBirth}  |  Phone Number: {this.PhoneNumber}  |  Email Address: {this.EmailAddress}  |  Major: {this.Major}";
+        }
+    }
+}
